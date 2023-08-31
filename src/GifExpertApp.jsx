3bce2257c9ec61322b1a0ef9b7d747db2b68fe2f
braian <brasanluc123@gmail.com>
2023-08-31 13:29:14 -0300

    GIFGrid

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,28 +1,26 @@
 import React from "react";
 import { useState } from "react";
 import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
 
 export const GifExpertApp = () => {
   const [categories, setCategories] = useState(["One Punch ", "Dragon ball"]);
 
   const onAddCategory = (newCategory) => {
-    console.log(newCategory);
+    if (categories.includes(newCategory)) return;
+
+    setCategories([newCategory, ...categories]);
   };
 
   return (
     <>
       <h1>Hola mundo</h1>
 
-      <AddCategory
-        //  setCategories={setCategories}
-        onNewCategory={(value) => onAddCategory(value)}
-      />
+      <AddCategory onNewCategory={(value) => onAddCategory(value)} />
 
-      <ol>
-        {categories.map((category) => {
-          return <li key={category}>{category}</li>;
-        })}
-      </ol>
+      {categories.map((category) => (
+        <GifGrid key={category} category={category} />
+      ))}
     </>
   );
 };
