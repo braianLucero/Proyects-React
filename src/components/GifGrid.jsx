c093ba114890ade50727b752fe051b48fad11f79
braian <brasanluc123@gmail.com>
2023-09-04 11:36:13 -0300

    display image titles by id

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getGif } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
+  const [images, setImages] = useState([]);
+
+  const getImages = async () => {
+    const newImages = await getGif(category);
+    setImages(newImages);
+  };
+
+  useEffect(() => {
+    getImages();
+  }, []);
+
   return (
     <>
       <h3>{category}</h3>
-      <p>Hola Mundo</p>
+
+      <ol>
+        {images.map(({ id, title }) => (
+          <li key={id}>{title}</li>
+        ))}
+      </ol>
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { GifExpertApp } from "./GifExpertApp";
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <GifExpertApp />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <GifExpertApp />
+  // </React.StrictMode>
 );
