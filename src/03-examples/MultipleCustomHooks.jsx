dbc6ccdeff65395b7b79dfcb7f44676a90d4c4f1
braian <brasanluc123@gmail.com>
2023-10-11 19:45:46 -0300

    useFetch + userCounter

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { useFetch } from "../hooks/useFetch";
 
 export const MultiCustomHooks = () => {
+  const { data, isLoading, haserror } = useFetch(
+    "https://api.breakingbadquotes.xyz/v1/quotes/5"
+  );
+
+  console.log({ data, isLoading, haserror });
+
   return (
     <>
       <h1>breakingBad Quotes</h1>
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,5 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export const useFetch = (url) => {
-  return {};
+  const [state, setState] = useState({
+    data: null,
+    isLoading: true,
+    haserror: null,
+  });
+
+  const getFetch = async () => {
+    setState({
+      ...state,
+      isLoading: true,
+    });
+
+    const resp = await fetch(url);
+    const data = await resp.json(); // Invoca la función json() para obtener los datos
+
+    console.log(data);
+
+    setState({
+      data,
+      isLoading: false,
+      haserror: null,
+    });
+  };
+
+  useEffect(() => {
+    getFetch();
+  }, [url]);
+
+  return {
+    data: state.data,
+    isLoading: state.isLoading,
+    haserror: state.isLoading,
+  };
 };
