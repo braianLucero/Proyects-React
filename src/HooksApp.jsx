446361be1f4f444b546b38afe3137e1bc61b15f5
braian <brasanluc123@gmail.com>
2023-10-19 16:44:44 -0300

    useRef

--- src/HooksApp.jsx
+++ src/HooksApp.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { MultiCustomHooks } from "./03-examples/MultipleCustomHooks";
+import { FocusScreen } from "./04-useRef/FocusScreen";
+// import { MultiCustomHooks } from "./03-examples/MultipleCustomHooks";
 // import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
 // import { CounterApp } from "./01-useState/CounterApp";
 // import { SimpleForm } from "./02-useEffect/SimpleForm";
 
 export const HooksApp = () => {
-  return <MultiCustomHooks />;
+  return <FocusScreen />;
 };
