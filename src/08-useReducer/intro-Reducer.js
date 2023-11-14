0710d79428755d54d7f4370feb64efcc67bc3e71
braian <brasanluc123@gmail.com>
2023-11-14 13:15:14 -0300

    reducer

--- src/08-useReducer/intro-Reducer.js
+++ src/08-useReducer/intro-Reducer.js
@@ -1,15 +1,58 @@
-const initialState = [
+const estadoIncial = [
   {
     id: 1,
-    todo: "recolectar la pidra del alma ",
+    todo: "recolectar la piedra del alma ",
     done: false,
   },
 ];
 
-const todoReducer = (state = initialState, action) => {
+const todoReducer = (state = estadoIncial, action = {}) => {
+  if (action.type === "[Todo] add todo ") {
+    return [...state, action.payload];
+  }
+
   return state;
 };
 
 let todos = todoReducer();
 
-console.log(todos);
+const newTodo1 = {
+  id: 2,
+  todo: "recolectar la piedra del universo ",
+  done: false,
+};
+
+const addTodoAction = {
+  type: "[Todo] add todo ",
+  payload: newTodo1,
+};
+
+todos = todoReducer(todos, addTodoAction);
+
+const newTodo2 = {
+  id: 3,
+  todo: "recolectar la piedra de la tierra ",
+  done: false,
+};
+
+const addTodoAction2 = {
+  type: "[Todo] add todo ",
+  payload: newTodo2,
+};
+
+todos = todoReducer(todos, addTodoAction2);
+
+const newtodo4 = {
+  id: 4,
+  todo: "la base de todo  ",
+  done: false,
+};
+
+const addTodoAction4 = {
+  type: "[Todo] add todo ",
+  payload: newtodo4,
+};
+
+todos = todoReducer(todos, addTodoAction4);
+
+console.log({ state: todos });
