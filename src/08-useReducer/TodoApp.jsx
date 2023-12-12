2eddc63e6407288ae5c14b9bb7b16e49a9ac0bc7
braian <brasanluc123@gmail.com>
2023-12-12 11:31:35 -0300

    DeleteTodo

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -20,6 +20,13 @@ export const TodoApp = () => {
     dispatch(action);
   };
 
+  const handleDeleteTodo = (id) => {
+    dispatch({
+      type: "[TODO] remove Todo",
+      payload: id,
+    });
+  };
+
   return (
     <>
       <h1>
@@ -30,7 +37,7 @@ export const TodoApp = () => {
 
       <div className="row">
         <div className="col-7">
-          <TodoList todos={todos} />
+          <TodoList todos={todos} onDeleteTodo={handleDeleteTodo} />
         </div>
         <div className="col-5">
           <h4>Agregar Todo</h4>
--- src/08-useReducer/todoReducer.js
+++ src/08-useReducer/todoReducer.js
@@ -3,6 +3,9 @@ export const todoReducer = (inicialState = [], action) => {
     case "[TODO] Add Todo":
       return [...inicialState, action.payload];
 
+    case "[TODO] remove Todo":
+      return inicialState.filter((todo) => todo.id !== action.payload);
+
     default:
       return inicialState;
   }
